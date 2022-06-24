23fc1e3944b0ac00bb7bb6f347487f97baf2abcf
cyrilfind <cyrilfind@gmail.com>
2022-06-24 17:55:14 +0200

    super basic setup

--- app/src/main/java/io/cyrilfind/mojotest/MainActivity.kt
+++ app/src/main/java/io/cyrilfind/mojotest/MainActivity.kt
@@ -2,10 +2,21 @@ package io.cyrilfind.mojotest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.cyrilfind.mojotest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // read raw file
+        val raw = resources.openRawResource(R.raw.sample).bufferedReader().use { it.readText() }
+        
+        binding.mainTextView.text = raw
+        
     }
 }
